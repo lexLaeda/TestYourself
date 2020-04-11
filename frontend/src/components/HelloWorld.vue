467631b14aea0dd0d5dabb0b249a911de7de0867
AlexeyErmakov <lexlaeda30@gmail.com>
2020-04-11 15:13:37 +0300

    start

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button class="search__button" @click="callRestService()">CALL Spring Boot REST backend service</button>
+
+    <h3>{{ response }}</h3>
+  </div>
+</template>
+
+<script>
+  import api from "./backend-api";
+
+  export default {
+    name: 'hello',
+
+    data() {
+      return {
+        posts: [],
+        errors: [],
+      }
+    },
+    methods: {
+      // Fetches posts when the component is created.
+      callRestService() {
+        api.get(`hello`)
+                .then(response => {
+                  // JSON responses are automatically parsed.
+                  this.posts = response.data
+                })
+                .catch(e => {
+                  this.errors.push(e)
+                })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/components/backend-api.js
@@ -0,0 +1,13 @@
+import axios from 'axios/index'
+
+const AXIOS = axios.create({
+    baseURL: `/api`,
+    timeout: 1000
+});
+
+
+export default {
+    hello() {
+        return AXIOS.get(`/hello`);
+    }
+}
