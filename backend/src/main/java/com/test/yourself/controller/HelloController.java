c1e541403cf07f636b4f52db72285093be4e3552
AlexeyErmakov <lexlaeda30@gmail.com>
2020-04-12 16:56:51 +0300

    add cros access

--- backend/src/main/java/com/test/yourself/controller/HelloController.java
+++ backend/src/main/java/com/test/yourself/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.test.yourself.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*",allowedHeaders = "http://localhost:8080")
 public class HelloController {
+
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello() {
+        System.out.println("gegeg");
         return "HELLO EVGENIIA";
     }
 }
