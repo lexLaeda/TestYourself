4e06bdbc55739b99f97d484642ec82145778388e
Русских Евгения <russkih.evgeniya92@gmail.com>
2020-04-18 16:35:49 +0300

    add menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,13 +6,37 @@
         dark
         class="flex-grow-0"
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>TestYourself</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link class="white--text" to="/">TestYourself</router-link>
+      </v-toolbar-title>
     </v-app-bar>
 
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item-group
+            active-class="cyan--text text--darken-2"
+        >
+          <v-list-item v-for="(element, index) in menuList" :key="index" :to="element.path" link>
+            <v-list-item-title
+            >
+              {{ element.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-container>
-      <add-question></add-question>
+      <router-view></router-view>
     </v-container>
 
     <v-footer
@@ -39,7 +63,17 @@ export default {
   name: 'App',
 
   data: () => ({
-
-  }),
+    drawer: false,
+    menuList: [
+      {
+        title: 'Добавить предмет',
+        path: '/add-subject'
+      },
+      {
+        title: 'Добавить вопрос',
+        path: '/add-question'
+      }
+    ]
+  })
 };
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
 import vuetify from './plugins/vuetify';
 
 import "./scss/main.scss";
 
 /*Components*/
-import AddQuestion from './components/AddQuestion/AddQuestion';
+import AddSubject from './components/AddSubject/AddSubject';
 
 Vue.config.productionTip = false
 
-Vue.component('add-question', AddQuestion);
+Vue.component('add-subject', AddSubject);
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
