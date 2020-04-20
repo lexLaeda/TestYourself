135e5eb059757417544f17acf0d584cba31e4003
AlexeyErmakov <lexlaeda30@gmail.com>
2020-04-20 23:03:44 +0300

    add new

--- backend/src/main/java/com/test/yourself/util/WikiParser.java
+++ backend/src/main/java/com/test/yourself/util/WikiParser.java
@@ -17,14 +17,18 @@ public class WikiParser {
     private String homeUrl;
     private String searchTag;
     @SneakyThrows
-    public String parseHtml(String request){
-        Connection connect = Jsoup.connect("https://ru.wikipedia.org/wiki/JavaScript");
+    public String parseHtml(){
+        Connection connect = Jsoup.connect("https://ru.wikipedia.org/wiki/javascript" );
         Document document = connect.get();
         Element body = document.body();
         Elements elementsByTag = body.getElementsByTag("p");
         String sb = elementsByTag.stream().map(Element::text).collect(Collectors.joining());
+        String[] split = sb.split(".");
         return sb;
     }
 
+    public static void main(String[] args) {
+        System.out.println(new WikiParser().parseHtml());
+    }
 
 }
