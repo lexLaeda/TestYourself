d2fa8baa14e97bd6f1b926f45936b641d21c1167
Русских Евгения <russkih.evgeniya92@gmail.com>
2020-04-20 22:41:21 +0300

    add generate

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
     <subject-list></subject-list>
+
+    <v-form
+      ref="form"
+      v-model="valid"
+    >
+      <v-text-field
+        v-model="postData.id"
+        label="Subject ID"
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="postData.number"
+        label="Subject ID"
+        outlined
+      ></v-text-field>
+      <v-btn @click="generate">Generate</v-btn>
+    </v-form>
+
     <br>
-    <br>
+
     <v-btn @click="hello">Hello</v-btn>
+
+    /generate?=id="1"&quant="2"
   </div>
 </template>
 
@@ -18,6 +38,12 @@
       SubjectList
     },
 
+    data: () => {
+      return {
+        postData: {}
+      }
+    },
+
     methods: {
       hello() {
         api.hello().then(response => {
@@ -26,6 +52,9 @@
           .catch(error => {
             console.log('ERROR =>', error);
           })
+      },
+      generate() {
+        api.generateTest(`/test/generate/?id=` + this.postData.id + `&number=` + this.postData.number);
       }
     }
   }
