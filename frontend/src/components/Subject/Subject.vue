6bcb18d9b4838b1b0d33e8ed3d04b05d021f8671
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-04-26 18:18:07 +0300

    add filters

--- frontend/src/components/Subject/Subject.vue
+++ frontend/src/components/Subject/Subject.vue
@@ -6,19 +6,55 @@
 
       <div class="mt-8">
         <template v-if="questions && questions.length > 0">
-          <div class="mt-6" v-for="(question, index) in questions" :key="index">
-            <v-hover>
-              <template v-slot="{ hover }">
-                <v-card
-                    :elevation="hover ? 24 : 6"
-                    class="mx-auto pa-6"
-                >
+          <form>
+            <v-text-field
+                v-model="filter"
+                label="Название вопроса"
+                outlined
+                append-icon="mdi-magnify"
+                @input="filterList"
+                hide-details
+            ></v-text-field>
+          </form>
+          <template v-if="filteredQuestions.length > 0">
+            <div class="mt-6" v-for="(question, index) in filteredQuestions" :key="index">
+              <v-card
+                class="mx-auto pa-6 elevation-6"
+              >
+                <div class="d-flex flex-nowrap justify-space-between">
                   <div class="title">{{ question.name }}</div>
-                  <div v-if="question.description" class="mt-3">{{ question.description }}</div>
-                </v-card>
-              </template>
-            </v-hover>
-          </div>
+                  <v-menu bottom left>
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                          icon
+                          v-on="on"
+                      >
+                        <v-icon>mdi-dots-vertical</v-icon>
+                      </v-btn>
+                    </template>
+
+                    <v-list>
+                      <v-list-item>
+                        <v-list-item-title>Редактировать</v-list-item-title>
+                      </v-list-item>
+                      <v-list-item>
+                        <v-list-item-title>Удалить</v-list-item-title>
+                      </v-list-item>
+                    </v-list>
+                  </v-menu>
+                </div>
+                <div v-if="question.description" class="mt-3">{{ question.description }}</div>
+              </v-card>
+            </div>
+          </template>
+          <template v-else>
+            <v-alert
+                color="red lighten-2"
+                dark
+            >
+              Вопросы не найдены
+            </v-alert>
+          </template>
         </template>
 
         <template v-else>
@@ -45,7 +81,9 @@
     data: () => ({
       dataLoad: false,
       subject: {},
-      questions: []
+      questions: [],
+      filter: '',
+      filteredQuestions: []
     }),
 
     methods: {
@@ -69,6 +107,7 @@
             if (response.status === 200) {
               let data = response.data;
               this.questions = data;
+              this.filteredQuestions = this.questions;
 
               this.dataLoad = true;
             }
@@ -76,6 +115,15 @@
           .catch(error => {
             console.log(error);
           });
+      },
+
+      filterList() {
+        if (this.filter !== '') {
+          this.filteredQuestions = this.questions
+            .filter(question => question.name.toLowerCase().includes(this.filter.toLowerCase()));
+        } else {
+          this.filteredQuestions = this.questions;
+        }
       }
     },
 
