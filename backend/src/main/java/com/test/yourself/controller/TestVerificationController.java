cfff732bbe150a2d7893e3e8527b1b50d0c7da79
AlexeyErmakov <lexlaeda30@gmail.com>
2020-04-27 17:43:43 +0300

    bug fix"

--- backend/src/main/java/com/test/yourself/controller/TestVerificationController.java
+++ backend/src/main/java/com/test/yourself/controller/TestVerificationController.java
@@ -12,9 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/test_verification")
+@RestController()
+@RequestMapping("/api/test_verification")
 public class TestVerificationController {
 
 
