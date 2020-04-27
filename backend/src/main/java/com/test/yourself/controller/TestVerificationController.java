df793fd38916d2778a03688fa1652c980a6fa832
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-04-27 18:17:46 +0300

    aaaa

--- backend/src/main/java/com/test/yourself/controller/TestVerificationController.java
+++ backend/src/main/java/com/test/yourself/controller/TestVerificationController.java
@@ -10,10 +10,7 @@ import com.test.yourself.service.test.TestResultService;
 import com.test.yourself.service.test.TestVerificationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/api/test_verification")
@@ -33,7 +30,7 @@ public class TestVerificationController {
         this.testResultService = testResultService;
     }
 
-    @GetMapping("/test_result")
+    @PostMapping("/test_result")
     public ResponseEntity<TestResultDto> verifySubjectTest(@RequestBody AnswerSheetDto answerSheetDto) {
 
         AnswerSheet answerSheet = answerSheetMapper.toEntity(answerSheetDto);
